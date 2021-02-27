90c6ea5cf150dd4d84cb6f5020b574dcde1f3940
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2021-02-27 13:16:39 +0530

    fist taste of compose layout system.

--- app/src/main/java/dev/saurabhmishra/composerestro/ui/splash/SplashScreen.kt
+++ app/src/main/java/dev/saurabhmishra/composerestro/ui/splash/SplashScreen.kt
@@ -1,8 +1,22 @@
 package dev.saurabhmishra.composerestro.ui.splash
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun SplashScreen() {
+  Box(modifier = Modifier.fillMaxSize()) {
+    Text(text = "Splash screen", modifier = Modifier.align(Alignment.Center))
+  }
+}
 
+@Composable
+@Preview(name = "Splash screen")
+fun SplashScreenPreview() {
+  SplashScreen()
 }
