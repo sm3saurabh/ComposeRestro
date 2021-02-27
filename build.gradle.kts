ac648bb60e0e78e71e794d518372eb19b23441e9
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2021-02-27 17:50:50 +0530

    add: hilt to classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,8 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:${Versions.GRADLE_PLUGIN_VERSION}")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN_VERSION}")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:${Versions.HILT_VERSION}")
+
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
