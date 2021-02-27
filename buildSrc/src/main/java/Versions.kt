6953b9e1ae7d918a0d37e4c418e0564fc1348c5d
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2021-02-27 17:50:31 +0530

    add: versions for coroutines, hilt, lifecycle and ViewModel compose specific dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,4 +1,8 @@
 object Versions {
+  const val COROUTINES_VERSION = "1.4.2"
+  const val HILT_VERSION = "2.32-alpha"
+  const val LIFECYCLE_VERSION = "2.3.0"
+  const val VIEWMODEL_COMPOSE_VERSION = "1.0.0-alpha02"
   const val COMPOSE_NAVIGATION = "1.0.0-alpha08"
   const val GRADLE_PLUGIN_VERSION = "7.0.0-alpha08"
   const val KOTLIN_VERSION = "1.4.30"
