e16019945ea38f89e3c4fe977b8c4498c9e70451
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2021-02-27 22:59:41 +0530

    add: injection of initializers in the application

--- app/src/main/java/dev/saurabhmishra/composerestro/ComposeRestroApp.kt
+++ app/src/main/java/dev/saurabhmishra/composerestro/ComposeRestroApp.kt
@@ -2,6 +2,15 @@ package dev.saurabhmishra.composerestro
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import dev.saurabhmishra.composerestro.utils.initializers.InitializerSet
+import javax.inject.Inject
 
 @HiltAndroidApp
-class ComposeRestroApp: Application()
+class ComposeRestroApp: Application() {
+  @Inject lateinit var initializerSet: InitializerSet
+
+  override fun onCreate() {
+    super.onCreate()
+    initializerSet.initialize(this)
+  }
+}
